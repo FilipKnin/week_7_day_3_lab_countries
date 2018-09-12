544e61794d5b4afc3a98c2b503559eaf22950a4d
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-12 17:28:21 +0100

    Finalcommit

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -7,15 +7,78 @@ const ResultView = function (container) {
 ResultView.prototype.bindEvents = function () {
   PubSub.subscribe('Selectedcountry:object-loaded', (event) => {
     const countryObject = event.detail;
-    this.render(countryObject);
+    this.publishRegion(countryObject);
+    this.publishFlag(countryObject);
+    this.publishLanguages(countryObject);
 
 
   });
 };
 
-ResultView.prototype.render = function () {
-
+ResultView.prototype.publishRegion = function (countryObject) {
+  this.container.innerHTML = '';
+  const newParagraph = document.createElement('p');
+  newParagraph.textContent = `${countryObject.region}`
+  //add textContent to createdParagraph
+  this.container.appendChild(newParagraph);
 };
+
+ResultView.prototype.publishFlag = function (countryObject) {
+
+  const newParagraph = document.createElement('p');
+  this.container.appendChild(newParagraph);
+  const newImage = document.createElement('img');
+  newImage.src = `${countryObject.flag}`
+  
+  newParagraph.appendChild(newImage);
+};
+
+ResultView.prototype.publishLanguages = function (countryObject) {
+
+  const newList = document.createElement('ol');
+  this.container.appendChild(newList);
+
+  const countryLanguages = countryObject.languages;
+
+  for (language of countryLanguages) {
+    let newElement = document.createElement('ul');
+    newElement.textContent = `${language.name}`;
+    newList.appendChild(newElement);
+  }
+
+  // countryObject.languages[i].name;
+
+
+
+  // document.createElement('img');
+  // newImage.src = `${countryObject.flag}`
+  // //add textContent to createdParagraph
+  // newParagraph.appendChild(newImage);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // InstrumentFamilyView.prototype.render = function (family) {
 //   this.container.innerHTML = '';
